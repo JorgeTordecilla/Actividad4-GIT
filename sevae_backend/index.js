4df5882ed097fb136c04f480415d1f9bbfcaa002
Jorge Tordecilla <jtordeci@ibero.edu.co>
2023-05-27 22:45:16 -0500

    firts commit

---
+++ sevae_backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo http://localhost:${PORT}`)
+);
