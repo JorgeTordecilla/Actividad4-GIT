700abfe8f391b8ed2a6191548f135395ea2ab19b
Jorge Tordecilla <jtordeci@ibero.edu.co>
2023-05-27 22:46:44 -0500

    added database config adn conn

--- sevae_backend/index.js
+++ sevae_backend/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
+const { dbConnection } = require("./database/config");
+
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const app = express();
 require("./swaggerConfig")(app);
 
+dbConnection();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
